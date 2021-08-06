4604b475f42028e4f71496b27ae00d58e9b24021
Zach McCleaf <anton@Zachs-MacBook-Air.local>
2021-08-06 10:49:39 -0400

    removing logic from main file into controller

--- main.go
+++ main.go
@@ -1,149 +1,22 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/zachmccleaf/todo-api/controllers"
 	"github.com/zachmccleaf/todo-api/helper"
-	"github.com/zachmccleaf/todo-api/models"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-
-var collection = helper.ConnectDB()
-
-func getTodos(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var todos []models.Todo
-
-	cur, err := collection.Find(context.TODO(), bson.M{})
-
-	if err != nil {
-		helper.GetError(err, w)
-		return
-	}
-
-	defer cur.Close(context.TODO())
-
-	for cur.Next(context.TODO()) {
-
-		var todo models.Todo
-		err := cur.Decode(&todo)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		todos = append(todos, todo)
-	}
-
-	if err := cur.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	json.NewEncoder(w).Encode(todos)
-}
-
-func getTodo(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var todo models.Todo
-	var params = mux.Vars(r)
-
-	id, _ := primitive.ObjectIDFromHex(params["id"])
-
-	filter := bson.M{"_id": id}
-	err := collection.FindOne(context.TODO(), filter).Decode(&todo)
-
-	if err != nil {
-		helper.GetError(err, w)
-		return
-	}
-
-	json.NewEncoder(w).Encode(todo)
-}
-
-func createTodo(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var todo models.Todo
-
-	_ = json.NewDecoder(r.Body).Decode(&todo)
-
-	result, err := collection.InsertOne(context.TODO(), todo)
-
-	if err != nil {
-		helper.GetError(err, w)
-		return
-	}
-
-	json.NewEncoder(w).Encode(result)
-}
-
-func updateTodo(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var params = mux.Vars(r)
-
-	id, _ := primitive.ObjectIDFromHex(params["id"])
-
-	var todo models.Todo
-
-	filter := bson.M{"_id": id}
-
-	_ = json.NewDecoder(r.Body).Decode(&todo)
-
-	update := bson.D{
-		{"$set", bson.D{
-			{"isbn", todo.Isbn},
-			{"message", todo.Message},
-			{"color", todo.Color},
-			{"isComplete", todo.IsComplete},
-		}},
-	}
-
-	err := collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&todo)
-
-	if err != nil {
-		helper.GetError(err, w)
-		return
-	}
-
-	todo.ID = id
-
-	json.NewEncoder(w).Encode(todo)
-}
-
-func deleteTodo(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var params = mux.Vars(r)
-
-	id, err := primitive.ObjectIDFromHex(params["id"])
-
-	filter := bson.M{"_id": id}
-
-	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
-
-	if err != nil {
-		helper.GetError(err, w)
-		return
-	}
-
-	json.NewEncoder(w).Encode(deleteResult)
-}
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/todos", getTodos).Methods("GET")
-	r.HandleFunc("/api/todos/{id}", getTodo).Methods("GET")
-	r.HandleFunc("/api/todos", createTodo).Methods("POST")
-	r.HandleFunc("/api/todos/{id}", updateTodo).Methods("PUT")
-	r.HandleFunc("/api/todos/{id}", deleteTodo).Methods("DELETE")
+	r.HandleFunc("/api/todos", controllers.GetTodos).Methods("GET")
+	r.HandleFunc("/api/todos/{id}", controllers.GetTodo).Methods("GET")
+	r.HandleFunc("/api/todos", controllers.CreateTodos).Methods("POST")
+	r.HandleFunc("/api/todos/{id}", controllers.UpdateTodos).Methods("PUT")
+	r.HandleFunc("/api/todos/{id}", controllers.UpdateTodos).Methods("DELETE")
 
 	config := helper.GetConfiguration()
 	log.Fatal(http.ListenAndServe(config.Port, r))
